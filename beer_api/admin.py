959d9684c50e2f9a4ded6101dd122b69639774bd
nickoloaded <essiendominic1@outlook.com>
2020-03-28 23:51:37 +0100

    Added Beer Model and migrations

--- beer_api/admin.py
+++ beer_api/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from beer_api import models
 
 # Register your models here.
+admin.site.register(models.BeerDiaryItem)
+
--- beer_api/models.py
+++ beer_api/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+SERVINGTYPE = (
+    (0, "Bottle"),
+    (1, "Draft")
+)
+
+class BeerDiaryItem(models.Model):
+    """Beer Diary Item """
+    beer_name = models.CharField(max_length=50)
+    brewer = models.CharField(max_length=50)
+    created_on = models.DateTimeField(auto_now_add=True)
+    price = models.FloatField(null=True, blank=True, default=0.0)
+    serving_type = models.IntegerField(choices=SERVINGTYPE, default=0)
+    description = models.CharField(max_length=255)
+
+    def __str__(self):
+        """Return the model as a string"""
+        return self.beer_name
